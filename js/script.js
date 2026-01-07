12ad2d656d59884c1d6fc7543fce485acf4d5512
Bianca Fialho <brgf@cesar.school>
2026-01-07 16:32:19 -0300

    Primeiro commit do projeto

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.getElementById("enviar").addEventListener("click", validarFormulario);
+
+//HTML
+ //<form>
+    //<label for="nome">Nome:</label>
+    //<input id="nome" type="text">
+    //<label for="email">Email:</label>
+    //<input id="email" type="email">
+    //<label for="telefone">Telefone:</label>
+    //<input id="telefone" type="number">
+    //<button id="botaoEnviar">Enviar</button>
+ //</form>
+
+//JS
+function validarFormulario() {
+
+  if(document.getElementById("nome").value !== "" && document.getElementById("email").value !== "")
+    {alert("Prontinho! Você receberá as novidades por email.");
+  }
+  else{
+    alert("Por favor, preencha os campos nome e email")
+  }
+
+};
+
+
